dc38d44f97658076437b044668cb55163d7464c5
weihuchao <weihuchao@xuetangx.com>
2019-07-19 19:32:44 +0800

    update setup

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 
 This is some django 1.9.9 utils, make it convenient to work.  
+
+
+## 1 get all urls
+
+
+```shell
+cd project_home
+python manage.py shell
+from django_utls import get_all_urls
+print get_all_urls()
+
+```
--- setup.py
+++ setup.py
@@ -2,19 +2,31 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2019-07-19 18:52
 # @Author  : weihuchao
+import os
 
 import setuptools
+
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
+
 
 setuptools.setup(
     name="django_utils",
     version="0.0.1",
+    python_requires="python 2.7",
     author="weihuchao",
     author_email="weihuchao@gmail.com",
     description="django 1.9.9 utils",
+    long_description=read('README.md'),
     url="https://github.com/weihuchao/django_utils",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 2.7",
-        "License :: MIT License",
-    ]
+        'Development Status :: 0.0.1 ',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+    ],
 )
